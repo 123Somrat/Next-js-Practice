b53be49285f418a864f22841fdacba819fa15ba3
123Somrat <jafaruddin875@gmail.com>
2024-03-30 12:14:59 +0100

    show single post

--- app/dashboard/books/[id]/page.js
+++ app/dashboard/books/[id]/page.js
@@ -1,8 +1,48 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 
 export default function page({params}) {
-    console.log(params.id)
+ 
+    const postId = params.id;
+    const [post,setPost]= useState({
+         id : '',
+         userId : '',
+         title : '',
+         body : ''
+    })
+
+   // destructure element from post
+    const {userId , title , body} = post;
+ 
+ 
+    // fetching data from jsonPlace holder
+useEffect(()=>{
+   
+  const fetchSinglePost = ()=>{
+     
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    .then(res=>res.json())
+    .then(data=>setPost(data))
+    .catch(err=>{
+       console.log(err)
+    })
+
+  }
+
+  fetchSinglePost()
+
+
+},[])
+
+
+
   return (
-    <div>{params.id}</div>
+    <div className='w-[450px] mx-auto border-2 border-white p-4 text-justify'>
+         <h1>UserId : {userId}</h1>
+         <h2>Tittle : {title}</h2>
+         <p>Body : {body}</p>
+
+    </div>
   )
 }
